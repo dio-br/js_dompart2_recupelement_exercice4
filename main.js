9101cbd18b44401273a5160aa5ae494c90f72015
Mohammed <el-med7@outlook.com>
2021-01-28 10:39:33 +0100

    fin

---
+++ main.js
@@ -0,0 +1,35 @@
+// 1
+
+console.log("Exo 1 : ");
+let redPurple = document.getElementsByClassName("redPurple");
+console.log(redPurple);
+
+// 2
+
+console.log("Exo 2 : ");
+let redPurple2 = document.querySelectorAll(".redPurple");
+console.log(redPurple2);
+
+// 3
+
+console.log("Exo 3 : ");
+let recupH1 = document.querySelectorAll("h1.redPurple")[0];
+console.log(recupH1);
+
+// 4
+
+console.log("Exo 4 : ");
+let exo4 = document.querySelectorAll("li, p, span");
+console.log(exo4);
+
+// 5
+
+console.log("Exo 5 : ");
+let exo5 = document.querySelectorAll("li.important, p");
+console.log(exo5);
+
+// 6
+
+console.log("Exo 6 : ");
+let exo6 = document.querySelectorAll("h1.redPurple, span.redPurple");
+console.log(exo6);
